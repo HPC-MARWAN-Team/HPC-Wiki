ff8150783dc67bfdc2b91ed981aece2ecba1389c
bahtisaadia <79312691+bahtisaadia@users.noreply.github.com>
2021-11-23 11:02:12 +0100

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,65 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'HPC-MARWAN'
+copyright = '2021, MARWAN team'
+author = 'MARWAN team'
+
+
+# -- General configuration ---------------------------------------------------
+
+
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# -- Options for LaTeX output ---------------------------------------------
+
+
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+
+html_static_path = ['_static']
+
+html_logo = "source/figures/logo_HPC-MARWAN_1.png"
+
+html_show_sourcelink = True
---
+++ index.rst
@@ -0,0 +1,21 @@
+
+.. HPC-MARWAN documentation master file, created by
+   sphinx-quickstart on Thu Sep 30 14:40:43 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Le wiki HPC-MARWAN
+******************
+
+Bienvenue sur le wiki  de calcul haute performance (HPC) du centre national pour le recherche scientifique et technique.
+
+
+Sommaire
+=================
+
+.. toctree::
+   :maxdepth: 2
+      
+   Guide utilisateur <source/guideutilsateur/indexguide.rst>
+   Tutoriel <source/tutoriel/tuto.rst>
+   FAQ <source/faq.rst>
